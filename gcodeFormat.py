834b023e4a2e109b825a4ef544402f0752aebe66
Eugene Sanduelnko <sandulenko.evgeniy@gmail.com>
2020-11-22 17:10:35 -0500

    Print message with type

--- gcodeFormat.py
+++ gcodeFormat.py
@@ -8,25 +8,33 @@ source_file_name = sys.argv[1]
 splitName = os.path.splitext(source_file_name)
 newFileName = splitName[0] + '_m' + splitName[1]
 
-print('Source file name: ', source_file_name)
-print('New file name: ', newFileName)
+print('Source file name: {0}'.format(source_file_name))
+print('New file name: {0}'.format(newFileName))
 
 sourceFile = open(source_file_name, 'r')
 sourceContent = sourceFile.readlines()
 sourceFile.close()
 
-print('Source file has  ', len(sourceContent), ' lines')
+print('Source file has {0} lines'.format(len(sourceContent)))
 
 layer_count = -1
-# writ new file
+current_layer = -1
+current_type = ''
+current_layer_message = ''
+# write new file
 with open(newFileName, 'w') as f:
     for line in sourceContent:
         f.write(line)
         if layer_count < 0 and line.startswith(';Layer count:'):
             layer_count = int(line[14:-1])
-            print('layers count: ', layer_count)
+            print('layers count: {0}'.format(layer_count))
         if line.startswith(';LAYER:'):
+            current_layer = line[1:-1]
+            current_type = ''
+            current_layer_message = 'M117 {0}\n'.format(current_layer)
             if layer_count > 0:
-                f.write('M117 {0} of {1}\n'.format(line[1:-1], layer_count))
-            else:
-                f.write('M117 {0}'.format(line[1:]))
+                current_layer_message = 'M117 {0} of {1}\n'.format(current_layer, layer_count, current_type)
+            f.write(current_layer_message)
+        if line.startswith(';TYPE:'):
+            current_type = line[6:]
+            f.write('{0} {1}'.format(current_layer_message[:-1], current_type))
--- gcodeFormatTest.py
+++ gcodeFormatTest.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 
-class TestGcodeFormater(unittest.TestCase):
+class TestGcodeFormatter(unittest.TestCase):
     test_file_name = 'testFile.txt'
     new_file_name = 'testFile_m.txt'
 
@@ -16,21 +16,21 @@ class TestGcodeFormater(unittest.TestCase):
         with  open(self.test_file_name, 'w') as source_file:
             source_file.writelines(content)
 
-    def run_gcode_formater(self):
+    def run_gcode_formatter(self):
         os.system('gcodeFormat.py ' + self.test_file_name)
 
-    def test_gcodeFormater_creates_new_file(self):
+    def test_gcodeFormatter_creates_new_file(self):
         file_content = ["File with gcode\n", "Second line\n", "Third line"]
         self.create_test_file(file_content)
 
-        self.run_gcode_formater()
+        self.run_gcode_formatter()
 
         with open(self.new_file_name, 'r') as new_file:
             new_file_content = new_file.readlines()
 
         self.assertEqual(new_file_content, file_content)
 
-    def test_gcodeFormater_adds_message_for_each_layer(self):
+    def test_gcodeFormatter_adds_message_for_each_layer(self):
         file_content = [
             "File with gcode\n",
             ";LAYER:0\n",
@@ -52,14 +52,45 @@ class TestGcodeFormater(unittest.TestCase):
 
         self.create_test_file(file_content)
 
-        self.run_gcode_formater()
+        self.run_gcode_formatter()
 
         with open(self.new_file_name, 'r') as new_file:
             new_file_content = new_file.readlines()
 
         self.assertEqual(new_file_content, expected_content)
 
-    def test_gcodeFormater_adds_message_for_each_layer_with_total_layers_number(self):
+    def test_gcodeFormatter_adds_message_for_each_layer_with_total_layers_number(self):
+        file_content = [
+            "File with gcode\n",
+            ";Layer count: 356\n",
+            ";LAYER:0\n",
+            ";LAYER:1\n",
+            ";LAYER:238\n",
+            "some code\n",
+            "Third line"]
+
+        expected_content1 = [
+            "File with gcode\n",
+            ";Layer count: 356\n",
+            ";LAYER:0\n",
+            "M117 LAYER:0 of 356\n",
+            ";LAYER:1\n",
+            "M117 LAYER:1 of 356\n",
+            ";LAYER:238\n",
+            "M117 LAYER:238 of 356\n",
+            "some code\n",
+            "Third line"]
+
+        self.create_test_file(file_content)
+
+        self.run_gcode_formatter()
+
+        with open(self.new_file_name, 'r') as new_file:
+            new_file_content = new_file.readlines()
+
+        self.assertEqual(new_file_content, expected_content1)
+
+    def test_gcodeFormatter_adds_message_with_type(self):
         file_content = [
             "File with gcode\n",
             ";Layer count: 356\n",
@@ -83,7 +114,7 @@ class TestGcodeFormater(unittest.TestCase):
 
         self.create_test_file(file_content)
 
-        self.run_gcode_formater()
+        self.run_gcode_formatter()
 
         with open(self.new_file_name, 'r') as new_file:
             new_file_content = new_file.readlines()
